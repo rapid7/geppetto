fa3fdedbfdac327c26132ff3554a111b6fc723a9
Jeffrey Martin <Jeffrey_Martin@rapid7.com>
2017-08-10 14:22:16 -0500

    create objects using new namespace

--- autoPayloadTest.py
+++ autoPayloadTest.py
@@ -94,9 +94,9 @@ def createServer(configFile, logFile = "default.log"):
         print "INVALID CONFIG FILE; NO HYPERVISOR_TYPE FOUND"
         return None
     if hypervisorDic['HYPERVISOR_TYPE'].lower() == "esxi":
-        return esxiVm.createFromConfig(hypervisorDic, logFile)
+        return vm_automation.esxiServer.createFromConfig(hypervisorDic, logFile)
     if hypervisorDic['HYPERVISOR_TYPE'].lower() == "workstation":
-        return workstationVm.workstationServer(hypervisorDic, logFile)
+        return vm_automation.workstationServer(hypervisorDic, logFile)
 
 def expandGlobalList(hostList, globalList, listName):
     for target in hostList:

f8e3dc613ff912180d3fa29cfe98eef1abe17f73
Jeffrey Martin <Jeffrey_Martin@rapid7.com>
2019-09-24 15:52:03 -0500

    add nightly test helper

---
+++ helpers/runTestsForPayload.py
@@ -0,0 +1,85 @@
+##############################################################
+#
+# THIS IS A WRAPPER SCRIPT FOR autoPayloadTest.py
+# IT TAKES IN A TEST FILE WITH PAYLOADS AND RUNS THEM
+# INDIVIDUALLY AGAINST A COMMON CONFIG FILE
+# RUN THIS IN THE SAME FOLDER AS autopayloadTtest.py
+#
+#############################################################
+
+import argparse
+import subprocess
+import json
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", help="Echo test result to console", action="store_true")
+    parser.add_argument("-f", "--framework", help="Framework branch to use (Overrides testfile)")
+    parser.add_argument("-m", "--module", help="Module to use")
+    parser.add_argument("testfile", help="json payload file to use")
+    args = parser.parse_args()
+
+    testFilename = args.testfile.strip()
+
+    try:
+        payloadFileObj = open(testFilename, 'r')
+        jsonStr = payloadFileObj.read()
+        payloadFileObj.close()
+    except IOError as e:
+        print("FAILED TO OPEN OR READ" + testFilename + "\n" + str(e))
+    try:
+        payloadList = json.loads(jsonStr)
+    except ValueError as f:
+        print("FAILED TO PARSE JSON FILE " + testFilename + "\n" + str(f))
+
+    if args.framework != None:
+        frameworkOption = " -f " + args.framework
+    else:
+        frameworkOption = ""
+
+    if args.module != None:
+        moduleOption = " -m " + args.module
+    else:
+        moduleOption = ""
+
+    if args.verbose != None:
+        verboseOption = " -v "
+    else:
+        verboseOption = ""
+
+    nightlyTestFile = 'public_configs/nightly_tests.json'
+    nightlyConfigs = None
+    try:
+        fileObj = open(nightlyTestFile, 'r')
+        jsonStr = fileObj.read()
+        fileObj.close()
+    except IOError as e:
+        print("FAILED TO OPEN OR READ" + nightlyTestFile + "\n" + str(e))
+    try:
+        nightlyConfigs = json.loads(jsonStr)
+    except ValueError as f:
+        print("FAILED TO PARSE JSON FILE " + nightlyTestFile + "\n" + str(f))
+
+    cmdList = []
+    for config in nightlyConfigs:
+        if testFilename in config['test_config']:
+            for payload in payloadList:
+                cmdString = "python autoPayloadTest.py -SS -p " + payload.strip() + " " + config['test_config']
+                cmdString = cmdString + frameworkOption
+                cmdString = cmdString + moduleOption
+                cmdString = cmdString + verboseOption
+                cmdList.append(cmdString)
+
+    failures = 0
+    for cmd in cmdList:
+        print cmd
+        if subprocess.call(cmd.split()) == 0:
+            print("PASSED")
+        else:
+            failures += 1
+            print("FAILED")
+    if failures > 0:
+        exit(1)
+    
+if __name__ == "__main__":
+    main()
